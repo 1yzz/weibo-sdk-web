959946d2a1bf006c9d1f0f8b45cb897e446c8271
1yzz <10379219+1yzz@users.noreply.github.com>
2019-06-19 10:57:00 +0800

    [Add] post text

--- tests/test_weibo.py
+++ tests/test_weibo.py
@@ -10,5 +10,7 @@ def test_get_username():
     assert wb.get_username() == '瞎眼看海贼'
 
 
-def test_new_post():
-    assert True
+def test_post_text():
+    res = wb.post_text('发布的微博lai[悲伤]')
+
+    assert res['msg'] == ''
--- weibo_web/api.py
+++ weibo_web/api.py
@@ -1,6 +1,21 @@
 import requests
 from .login import login
 from .user import get_username
+from .post import post_text
+
+
+def _retry(max_retry=100):
+    def fn(func):
+        def wrapper(self, *args, **kwargs):
+            try:
+                return func(self, *args, **kwargs)
+            except:
+                self.login()
+                return func(self, *args, **kwargs)
+
+        return wrapper
+
+    return fn
 
 
 class Weibo:
@@ -10,24 +25,20 @@ class Weibo:
         self.session = requests.session()
         self.uuid = 0
 
-    def _retry(func):
-        def wrapper(self, *args, **kwargs):
-            try:
-                return func(self, *args, **kwargs)
-            except:
-                self.login()
-                return func(self, *args, **kwargs)
-        return wrapper
-
     def login(self):
         self.uuid, self.session = login(self.username, self.password)
+        self.session.headers.update({
+            'Referer': 'https://weibo.com/u/%s/home?topnav=1&wvr=6' % self.uuid,
+            'Origin': 'https://weibo.com'
+        })
 
-    @_retry
+    @_retry()
     def get_username(self):
         return get_username(self.session, self.uuid)
 
-    def post_text(self):
-        pass
+    @_retry()
+    def post_text(self, text):
+        return post_text(self.session, text)
 
     def post_with_img(self):
         pass
@@ -37,5 +48,3 @@ class Weibo:
 
     def comment(self):
         pass
-
-
